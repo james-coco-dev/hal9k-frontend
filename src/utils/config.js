4e9b27a7b168cbb6cd3b19df4eebadae28159114
super-talent-aj <my.hercules.gj@gmail.com>
2020-11-16 07:57:24 -0800

    update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,1 +1,20 @@
 export const POOLS_KEY = "https://api.hal9k.ai/pools/";
+
+export const Artifact = {
+  rinkeby: {
+    hal9k: "0x80aCE96aB5a40F110c9477460c77004CA16669a2",
+    proxyAdmin: "0x6ea31a0ADEc3654F81EC7F3400dadD0D56eC3A2F",
+    hal9kVault: "0x95875951e653E0f37dA1d1e6E596ba4011192A78",
+    feeApprover: "0xBd78FAB43462837157E066b87808F79Cee122EC3",
+    hal9kLtd: "0x6aFb66f0D3188e400A4bBFA589CfF01E6c9F91b3",
+    hal9kNftpool: "0x2e0Ee634bBF62dF4ad1B444Faf1163320Cbf81dF",
+  },
+  mainnet: {
+    hal9k: "",
+    proxyAdmin: "",
+    hal9kVault: "",
+    feeApprover: "",
+    hal9kLtd: "",
+    hal9kNftpool: "",
+  },
+};
