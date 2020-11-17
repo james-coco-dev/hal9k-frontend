815c72f863e4b647b767ffb7e825926e33057cff
super-talent-aj <my.hercules.gj@gmail.com>
2020-11-16 19:06:07 -0500

    add LGE deposit

--- src/pages/Event.vue
+++ src/pages/Event.vue
@@ -15,6 +15,16 @@
             <div>$ {{ halPrice }} HAL9K Price Estimate after LGE</div>
             <div>{{ marketCap }} Market Cap</div>
           </div>
+          <div class="deposit-box">
+            <div>Deposit ETH: </div>
+            <input 
+              class="deposit-input" 
+              id="deposit-input" 
+              @keypress="onlyNumber"
+              v-model="ethToDeposit" 
+              v-on:keyup.enter="addLiquidity"
+            />
+          </div>
           <div v-if="address">
             <div class="agree-container">
               <input type="checkbox" v-model="agree" id="agree-box" />
@@ -23,10 +33,10 @@
                 any kind. I agree to not hold the contract creators, HAL9K team
                 members or anyone associated with this event liable for any
                 damage monetary and otherwise I might occur. I understand that
-                any smart contract interaction carries an inherent risk.</label
-              >
+                any smart contract interaction carries an inherent risk.
+              </label>
             </div>
-            <button class="liquidity-add-but" :disabled="!agree">
+            <button class="liquidity-add-but" :disabled="!agree" v-on:click="addLiquidity">
               ADD LIQUIDITY AND GET LP TOKENS
             </button>
           </div>
@@ -63,101 +73,154 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
-export default {
-  data: () => ({
-    agree: false,
-    value: 0,
-    timestamp: 0,
-    currentTimestamp: 0,
-    liquidityEnds: 7 * 24 * 60 * 60,
-    day: 0,
-    hour: 0,
-    min: 0,
-    sec: 0,
-    totalEthContributed: 0,
-    halPrice: 0,
-    marketCap: 0,
-  }),
-  computed: {
-    ...mapState({
-      address: (state) => state.account.address,
+  import { mapState } from "vuex";
+  import Metamask from '../utils/Metamask';
+  import store from "../store";
+  import { Artifact } from '../utils/config';
+  import Web3 from 'web3';
+
+  export default {
+    data: () => ({
+      agree: false,
+      value: 0,
+      timestamp: 0,
+      currentTimestamp: 0,
+      liquidityEnds: 7 * 24 * 60 * 60,
+      day: 0,
+      hour: 0,
+      min: 0,
+      sec: 0,
+      totalEthContributed: 0,
+      halPrice: 0,
+      marketCap: 0,
+      ethToDeposit: 0,
     }),
-    isFinished() {
-      return (
-        this.timestamp &&
-        this.timestamp + this.liquidityEnds < this.currentTimestamp
-      );
+
+    computed: {
+      ...mapState({
+        address: (state) => state.account.address,
+      }),
+      isFinished() {
+        return (
+          this.timestamp &&
+          this.timestamp + this.liquidityEnds < this.currentTimestamp
+        );
+      },
     },
-  },
-  methods: {
-    retrieveTimestamp() {
-      this.currentTimestamp = Math.round(new Date().getTime() / 1000);
-      const leftSecs =
-        this.timestamp + this.liquidityEnds - this.currentTimestamp;
-      this.value = Math.floor(
-        ((this.currentTimestamp - this.timestamp) / this.liquidityEnds) * 100
-      );
-      this.sec = leftSecs % 60;
-      this.min = Math.floor(leftSecs / 60) % 60;
-      this.hour = Math.floor(leftSecs / 60 / 60) % 24;
-      this.day = Math.floor(leftSecs / 60 / 60 / 24);
-      setTimeout(() => this.retrieveTimestamp(), 1000);
+
+    methods: {
+      retrieveTimestamp() {
+        this.currentTimestamp = Math.round(new Date().getTime() / 1000);
+        const leftSecs = this.timestamp + this.liquidityEnds - this.currentTimestamp;
+        this.value = Math.floor(((this.currentTimestamp - this.timestamp) / this.liquidityEnds) * 100);
+        this.sec = leftSecs % 60;
+        this.min = Math.floor(leftSecs / 60) % 60;
+        this.hour = Math.floor(leftSecs / 60 / 60) % 24;
+        this.day = Math.floor(leftSecs / 60 / 60 / 24);
+        setTimeout(() => this.retrieveTimestamp(), 1000);
+      },
+      onlyNumber ($event) {
+        //console.log($event.keyCode); //keyCodes value
+        let keyCode = ($event.keyCode ? $event.keyCode : $event.which);
+        if ((keyCode < 48 || keyCode > 57) && keyCode !== 46) { // 46 is dot
+            $event.preventDefault();
+        }
+      },
+      async addLiquidity ($event) {
+        if (!this.agree) return;
+        if (parseFloat(this.ethToDeposit) === 0) return;
+        const { address } = store.state.account;
+        if (Metamask.hal9k) {
+          try {
+            await Metamask.hal9k.methods.addLiquidity({ data: true, from: address, value: Metamask.web3.utils.toWei(this.ethToDeposit) }).call();
+            // let LiquidityAdditionEvent = Metamask.hal9k.LiquidityAuction();
+            // liquidityAuctionEvent.watch((err, result) => {
+            //   console.log(result);
+            //   if (result) {
+            //     console.log("Successfully deposited " + returnValues.value + " to " + returnValues.dst);
+            //   }
+            // })
+          } catch (e) {
+            console.log("Error", e);
+          }
+        }
+      }
     },
-  },
-  mounted() {
-    this.$store.commit("loading", true);
-    setTimeout(() => {
-      this.$store.commit("loading", false);
-      this.timestamp = 1605097493;
-      this.retrieveTimestamp();
-    }, 1500);
-  },
-};
+
+    async mounted() {
+      this.$store.commit("loading", true);
+      if (Metamask.hal9k) {
+        try {
+          const startTimestamp =  await Metamask.hal9k.methods.contractStartTimestamp().call();
+          this.timestamp = parseInt(startTimestamp);
+        } catch (e) {
+          console.log("Error");
+        }
+        this.$store.commit("loading", false);
+        this.retrieveTimestamp();
+      }
+    },
+  };
 </script>
+
 <style lang="scss" scoped>
-.event-container {
-  margin: 2rem 2rem;
-}
-.lge-box,
-.event-box {
-  padding: 1rem;
-  font-size: 1.2em;
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-}
-.left-day {
-  font-size: 20px;
-  text-align: left;
-  p {
-    font-weight: 500;
+  .event-container {
+    margin: 2rem 2rem;
   }
-}
-.info-box {
-  margin-top: 1rem;
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-}
-.agree-container {
-  margin-bottom: 20px;
-}
-.progress-box {
-  display: flex;
-  flex-direction: row;
-  gap: 20px;
-  progress {
-    flex: 1;
+  .lge-box,
+  .event-box {
+    padding: 1rem;
+    font-size: 1.2em;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
   }
-}
-.liquidity-add-but {
-  background: transparent;
-  border: 1px solid white;
-  color: white;
-  &:disabled {
-    border: 1px solid #fff3;
-    color: #fff3;
+  .left-day {
+    font-size: 20px;
+    text-align: left;
+    p {
+      font-weight: 500;
+    }
   }
-}
+  .deposit-box {
+    margin-bottom: 1rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .deposit-input {
+    width: 250px;
+    height: 25px;
+    text-align: right;
+    font-family: orbitron, sans-serif;
+    font-size: 17px;
+    border: 0px solid black; 
+  }
+  .info-box {
+    margin-top: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
+  .agree-container {
+    margin-bottom: 20px;
+  }
+  .progress-box {
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+    progress {
+      flex: 1;
+    }
+  }
+  .liquidity-add-but {
+    background: transparent;
+    border: 1px solid white;
+    color: white;
+    &:disabled {
+      border: 1px solid #fff3;
+      color: #fff3;
+    }
+  }
 </style>
--- src/utils/Metamask.js
+++ src/utils/Metamask.js
@@ -1,13 +1,27 @@
 import detectEthereumProvider from "@metamask/detect-provider";
+import { Artifact } from './config';
+import Hal9kJson from './abi/Hal9k.json';
+import Hal9kLtdJson from './abi/Hal9kLtd.json';
+import Hal9kNftPoolJson from './abi/Hal9kNftPool.json';
+import Hal9kVaultJson from './abi/Hal9kVault.json';
 import store from "../store";
+import Web3 from 'web3';
+
 class Web3Wrapper {
   constructor() {
     this.provider = null;
     this.chainId = null;
+    this.hal9k = null;
+    this.hal9kVault = null;
+    this.hal9kLtd = null;
+    this.hal9kNftPool = null;
+    this.web3 = null;
   }
+
   handleChainChanged = (_chainId) => {
     console.log("chainid========>", _chainId);
   };
+
   handleAccountsChanged = (accounts) => {
     console.log("address ===>", accounts);
     const { address } = store.state.account;
@@ -17,9 +31,11 @@ class Web3Wrapper {
       store.commit("account/setAddress", accounts[0]);
     }
   };
+
   disconnect = () => {
     store.dispatch("account/disconnect");
   };
+
   connect = async () => {
     try {
       const provider = await detectEthereumProvider();
@@ -42,6 +58,20 @@ class Web3Wrapper {
           console.error(err);
           store.dispatch("account/disconnect");
         });
+      
+      this.web3 = new Web3(this.provider);
+      
+      this.hal9k = new this.web3.eth.Contract(Hal9kJson, Artifact.rinkeby.hal9k);
+      this.hal9k.setProvider(this.provider);
+
+      this.hal9kLtd = new this.web3.eth.Contract(Hal9kLtdJson, Artifact.rinkeby.hal9kLtd);
+      this.hal9kLtd.setProvider(this.provider);
+
+      this.hal9kNftPool = new this.web3.eth.Contract(Hal9kNftPoolJson, Artifact.rinkeby.hal9kNftPool);
+      this.hal9kNftPool.setProvider(this.provider);
+
+      this.hal9kVault = new this.web3.eth.Contract(Hal9kVaultJson, Artifact.rinkeby.hal9kVault);
+      this.hal9kVault.setProvider(this.provider);
     } catch (err) {
       console.error(err);
       store.dispatch("account/disconnect");
@@ -49,5 +79,5 @@ class Web3Wrapper {
   };
 }
 
-const Web3 = new Web3Wrapper();
-export default Web3;
+const HalWeb3 = new Web3Wrapper();
+export default HalWeb3;
