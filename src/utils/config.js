5358ec380b05c702827b3cb5747c5c795727e2d7
super-talent-aj <my.hercules.gj@gmail.com>
2020-11-25 19:19:41 -0800

    update contract address

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,19 +1,23 @@
 require("dotenv").config();
 
-export const API_URL = process.env.NODE_ENV === "development" ? "http://0.0.0.0:8080" : 
-  process.env.NODE_ENV === 'production' ? "https://api.hal9k.ai" : "";
+export const API_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://0.0.0.0:8080"
+    : process.env.NODE_ENV === "production"
+    ? "https://api.hal9k.ai"
+    : "";
 console.log("API_URL : ", API_URL);
 
 export const POOLS_KEY = "https://api.hal9k.ai/pools/";
 
 export const Artifact = {
   rinkeby: {
-    hal9k: "0xf16447C1FD9BADEfa795Ab4EBF9b72DCBd91EF2B",
-    hal9kLtd: "0xeBB4e3EF1F7DaF0F241fc6e005562470cB8c7620",
-    hal9kVault: "0x3E7F103Eb996dA047137D7B55c8198628c3964b1",
-    hal9kNftPool: "0x1f493192909763D1B2688912Dae59E0371006151",
-    hal9kv1Router: "0xe3400365f90cf5442F997Cf7E230334025889973",
-    pairAddress: "0x3E9233eE45f9bF7E6Cff20DaA2D9C49a1A513F3D",
+    hal9k: "0x91d7f0e332fd463eC20a0Dfc4c13c56b9BA2b768",
+    hal9kLtd: "0x8Cf6726e12c8B3D799a6e0558fAe4671076a13Aa",
+    hal9kVault: "0xeBABb615a4B52114DBefE6E4830401df5f021Da2",
+    hal9kNftPool: "0xf59B03F0785C16ee706a7bfa59453E03CC1Fee72",
+    hal9kv1Router: "0x69D1BB385916Da08A4d922FF801948B390F484bA",
+    pairAddress: "0x4c94A60709D6FDf5A246BFA16ffd15c328C7eDe5",
   },
   mainnet: {
     hal9k: "",
@@ -25,7 +29,7 @@ export const Artifact = {
   },
 };
 
-// NOTE: This NFT config is all changeable 
+// NOTE: This NFT config is all changeable
 export const NFTConfig = {
   numberOfNFTsByClass: {
     common: [2500, 2500, 2500, 2500],
@@ -36,8 +40,8 @@ export const NFTConfig = {
   },
   // Stage number starts from 1. So you have to wait 1 day to get on stage 1.
   // and need to wait 2 more to get on stage 2 ...
-  // after stage 3, we'll use the same variable as in stage 3 
-  daysToWaitPerStage: [1, 2, 2], 
+  // after stage 3, we'll use the same variable as in stage 3
+  daysToWaitPerStage: [1, 2, 2],
   numberOfUpgradesPerStage: {
     stage1: [],
     stage2: [],
@@ -54,11 +58,10 @@ export const NFTConfig = {
     stage2: [4000, 100, 20, 2],
     stage2Total: 4122,
     stage3: [5000, 200, 40, 5],
-    stage3Total: 5245
-  }
+    stage3Total: 5245,
+  },
   // After user claims his NFT in certain stage, we should move him one stage back.
   // So we should enable them either claim NFT or Move to the next stage, not both at the same time.
 
   // We should have graphical interface that shows their progress and probabilities for their current stage.
-}
-
+};
